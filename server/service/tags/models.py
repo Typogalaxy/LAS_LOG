a8ec5273ce1d5845bf0f434f9401af82c559e26d
Andy <kuobuoo@gmail.com>
2023-10-27 11:56:16 +0800

    添加tags数据模型

--- server/service/tags/models.py
+++ server/service/tags/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from server.service.LAS_LOGs.models import Topic
 
 
 class Tag(models.Model):
     """标签"""
+    Topic = models.ForeignKey(Topic, models.CASCADE)
     text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """返回模型的字符串表示"""
