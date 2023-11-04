abde20f0bd534c0c30314f87d4585f5638c83e03
Andy <kuobuoo@gmail.com>
2023-11-04 21:41:56 +0800

    虚拟环境大更改

--- server/service/LAS_LOGs/urls.py
+++ server/service/LAS_LOGs/urls.py
@@ -3,14 +3,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics', views.topics, name='topics'),
-    path('topics/(?P<topic_id>\d+)', views.topic, name='topic'),
+    path('topics/(<topic_id>)', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
-    path('new_entry/(?P<topic_id>\d+)/', views.new_entry, name='new_entry'),
-    path('edit_entry/(?P<entry_id>\d+)/', views.edit_entry, name='edit_entry'),
+    path('new_entry/(<topic_id>)/', views.new_entry, name='new_entry'),
+    path('edit_entry/(<entry_id>)/', views.edit_entry, name='edit_entry'),
     path('links/', views.links, name='links'),
     path('new_link/', views.new_link, name='new_link'),
 ]
